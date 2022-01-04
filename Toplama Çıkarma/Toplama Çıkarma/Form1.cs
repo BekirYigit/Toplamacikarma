e3c85ed465a8e83ed065e3fe3f3b1dd1d454a9eb
BekirYigit <kbekiryigit@gmail.com>
2022-01-04 22:39:59 +0300

    sa
    as

---
+++ Toplama Çıkarma/Toplama Çıkarma/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Toplama_Çıkarma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int deger1 = Convert.ToInt32(textBox1.Text);
+            int deger2 = Convert.ToInt32(textBox2.Text);
+            if (radioButton1.Checked == true)
+            {
+                label3.Text = (deger1 + deger2).ToString();
+            }
+            else
+            {
+                label3.Text = (deger1 - deger2).ToString();
+            }
+        }
+    }
+}
